503eff683874eac6b0cc3058fc7ddd937ba79463
Alexa Hendry <alexahendry88@gmail.com>
2022-09-29 12:50:58 +0000

    Create Song model

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Song(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    artist = models.TextField(max_length=200)
+    album = models.TextField(max_length=200)
+    lyrics = models.TextField()
+
+    class Meta:
+        ordering = ['artist']
+
+    def __str__(self):
+        return self.title
