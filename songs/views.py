b3736ddda9ad81c8f64d5379cd83c896e59595de
Alexa Hendry <alexahendry88@gmail.com>
2022-09-29 15:40:45 +0000

    Create SongDetail view

--- songs/views.py
+++ songs/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Song
 
 
@@ -8,3 +8,23 @@ class SongList(generic.ListView):
     queryset = Song.objects.order_by('artist')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class SongDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        song = get_object_or_404(queryset, slug=slug)
+        artist = song.artist
+        album = song.album
+        lyrics = song.lyrics
+
+        return render(
+            request,
+            "song_detail.html",
+            {
+                "song": song,
+                "artist": artist,
+                "album": album,
+                "lyrics": lyrics
+            },
+        )
