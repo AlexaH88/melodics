c7d91a0a46c55f01c3462614fbae989058b5e7ed
Alexa Hendry <alexahendry88@gmail.com>
2022-09-30 13:37:35 +0000

    Install allauth, update urls.py, requirements.txt, add new nac links to base.html

--- melodics/urls.py
+++ melodics/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('songs.urls'), name='song_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
