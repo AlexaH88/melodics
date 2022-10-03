f9e2b24274d54d98d8ee9c3ee2ce50819b7031e8
Alexa Hendry <alexahendry88@gmail.com>
2022-10-03 16:29:39 +0000

    Add add_song post function on views.py

--- songs/forms.py
+++ songs/forms.py
@@ -5,4 +5,4 @@ from django import forms
 class SongForm(forms.ModelForm):
     class Meta:
         model = Song
-        fields = ('title', 'artist', 'album', 'lyrics',)
+        fields = ('title', 'artist', 'album', 'lyrics', 'slug',)
--- songs/views.py
+++ songs/views.py
@@ -28,7 +28,6 @@ class SongDetail(View):
                 "artist": artist,
                 "album": album,
                 "lyrics": lyrics,
-                "song_form": SongForm()
             },
         )
 
@@ -36,6 +35,25 @@ class SongDetail(View):
 class AddSong(View):
 
     def get(self, request):
+
+        return render(
+            request,
+            "add_song.html",
+            {
+                "song_form": SongForm()
+            },
+        )
+
+    def post(self, request):
+
+        song_form = SongForm(data=request.POST)
+
+        if song_form.is_valid():
+            song_form.instance.email = request.user.email
+            song_form.instance.name = request.user.username
+            song_form.save()
+        else:
+            song_form = SongForm()
 
         return render(
             request,
